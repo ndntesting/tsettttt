bf4cdd4cae0154b01972dfcd14554c0f5e14be71
ndntesting <ndntesting@outlook.com>
2018-06-04 17:53:18 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/tsettttt/*.csproj && dotnet build src/tsettttt/*.csproj
---
+++ src/tsettttt/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace tsettttt
+{
+    public class Class1
+    {
+    }
+}
